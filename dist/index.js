3bccdeec4586777d4e99378228cff087254725fd
Kim, Jang-hwan (김장환) <janghwan@gmail.com>
2024-01-21 20:27:08 +0900

    add dist/index.js  files

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+import plugin from './plugin.js';
+import { rehypeCodeHighlight } from './rehypeCodeHighlight.js';
+import { rehypeAnimateLists } from './rehypeAnimateLists.js';
+export { plugin as default, rehypeCodeHighlight, rehypeAnimateLists };
